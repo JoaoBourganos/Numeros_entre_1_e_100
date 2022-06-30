5c3d864e30eae12092ae146763a19c717868f9be
joao_ <joao_@PC30K>
2022-06-30 14:34:35 -0300

    Adicionar arquivos de projeto.

---
+++ Numeros_entre_1_e_100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros_entre_1_e_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeros;
+            for (numeros = -1; numeros < 100; numeros++)
+            {
+                numeros = numeros + 1;
+                Console.WriteLine(numeros);
+            }
+        Console.ReadKey();
+        }
+    }
+}
